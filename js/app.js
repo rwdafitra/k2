5b6bf54b99f0d58af26e213d69a6300df96ab2e9
Phoenix <31291691+Grimjo33@users.noreply.github.com>
2026-01-06 15:48:30 +0700

    a

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const supabaseUrl = 'https://ianpwqntmwqaycgvhukq.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhbnB3cW50bXdxYXljZ3ZodWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2NjcwMzcsImV4cCI6MjA4MzI0MzAzN30.A5f8Knc9Sgr56NGWlgAu72jVSXehR8Ew7xFuLBloYXcY';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhbnB3cW50bXdxYXljZ3ZodWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2NjcwMzcsImV4cCI6MjA4MzI0MzAzN30.A5f8Knc9Sgr56NGWlgAu72jVSXehR8Ew7xFuLBloYXc';
 
 window.supabaseClient = supabase.createClient(
   supabaseUrl,
@@ -18,17 +18,17 @@ supabaseClient.auth.onAuthStateChange((event, session) => {
 });
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const { data } = await supabaseClient.auth.getSession();
-  console.log('SESSION:', data);
+  const { data, error } = await supabaseClient.auth.getSession();
+  console.log('SESSION:', data, error);
 
   const user = data?.session?.user;
-
   const isLoginPage =
     location.pathname === '/' ||
     location.pathname.endsWith('index.html');
 
   if (!user && !isLoginPage) {
     location.replace('index.html');
+    return;
   }
 
   if (user && isLoginPage) {
