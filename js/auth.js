0e2a42427d6f395f8b3a29a34a212f45230111d2
Phoenix <31291691+Grimjo33@users.noreply.github.com>
2026-01-06 15:39:10 +0700

    a

--- js/auth.js
+++ js/auth.js
@@ -3,10 +3,12 @@ const form = document.getElementById('login-form');
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  const { error } = await supabaseClient.auth.signInWithPassword({
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
     email: emailInput.value,
     password: passwordInput.value
   });
+
+  console.log('LOGIN RESULT:', data, error);
 
   if (error) {
     info.innerText = error.message;
